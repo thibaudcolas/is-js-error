ed9599c3e533c872f2ea450a2c49b21010d61e1d
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-04 06:10:56 +0200

    Add output tests

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+COMMAND="node index.js"
+
+$COMMAND
+
+$COMMAND --version
+
+$COMMAND --help
+
+$COMMAND https://rawgit.com/ThibWeb/is-js-error/master/test/no-error.html
+echo $?
+
+$COMMAND https://rawgit.com/ThibWeb/is-js-error/master/test/error.html
+echo $?
