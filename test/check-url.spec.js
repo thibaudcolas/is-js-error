75c8cce25101857980bf4001abd62cb461e5d5d8
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-17 18:21:15 +0200

    Trying out local IP instead of localhost for Travis builds

--- test/check-url.spec.js
+++ test/check-url.spec.js
@@ -17,7 +17,7 @@ describe('Check URL', function testChechkURL() {
     it('should report correctly when there is no JS error', (done) => {
         const server = testServer.noErrorTest(4001);
 
-        checkURL('http://localhost:4001/', (err, result) => {
+        checkURL('http://127.0.0.1:4001/', (err, result) => {
             expect(err).to.equal(null);
             expect(result[1]).to.equal(false);
 
@@ -30,7 +30,7 @@ describe('Check URL', function testChechkURL() {
     it('should report correctly when there is a JS error', (done) => {
         const server = testServer.errorTest(4002);
 
-        checkURL('http://localhost:4002/', (err, result) => {
+        checkURL('http://127.0.0.1:4002/', (err, result) => {
             expect(err).to.equal(null);
             expect(result[1]).to.equal(true);
 
