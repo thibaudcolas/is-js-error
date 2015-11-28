e62ec09f9134da3d0f5f8f07377b250786de1c22
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-28 05:35:20 +0200

    Test wait parameter in output script

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ $COMMAND --help
 node -e "require('babel-core/register'); require('./test/server').default(4002);" &
 SERVER_PID=$!
 
+echo "No JS error"
 $COMMAND http://localhost:4002/no-error
 CODE=$?
 echo $CODE
@@ -19,6 +20,7 @@ then
     exit 1;
 fi
 
+echo "JS error"
 $COMMAND http://localhost:4002/error
 CODE=$?
 echo $CODE
@@ -27,10 +29,29 @@ then
     exit 1;
 fi
 
+echo "Load error"
 $COMMAND http://localhost:9999/notfound
 CODE=$?
 echo $CODE
 if [ $CODE -ne 1 ];
+then
+    exit 1;
+fi
+
+echo "No immediate JS error"
+$COMMAND http://localhost:4002/error-wait
+CODE=$?
+echo $CODE
+if [ $CODE -ne 0 ];
+then
+    exit 1;
+fi
+
+echo "JS error after 2500ms wait"
+$COMMAND http://localhost:4002/error-wait --wait 2500
+CODE=$?
+echo $CODE
+if [ $CODE -ne 2 ];
 then
     exit 1;
 fi
